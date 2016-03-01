48c6447f574c662a19c7dc03bce3e1de4c38770c
Mikhail Glukhov <arion2107@gmail.com>
2016-03-01 23:36:32 +0600

    add to JavaLearn paragraph 16 - object String

---
+++ src/Java_Learn_GS/Глава_16/SortString.java
@@ -0,0 +1,22 @@
+package Java_Learn_GS.Глава_16;
+
+/**
+ * Created by arion on 01.03.2016.
+ */
+class SortString {
+    static String arr[] = {"Now", "is", "the", "time", "for", "all", "good", "men", "to", "come", "to",
+        "the", "aid", "of", "their", "country"};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j].compareToIgnoreCase(arr[i]) < 0) {
+                    String t = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t;
+                }
+            }
+            System.out.println(arr[i]);
+        }
+    }
+}
